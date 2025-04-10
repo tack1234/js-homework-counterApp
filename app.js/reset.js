bae3a0c52e76e6e0ca4d2435c7c87f8881ab1866
tack1234 <tactak1313@gmail.com>
2025-04-10 22:54:20 +0900

    表示されるカウンターが0のとき、デフォルトのカラーになるように修正しました。

--- app.js/reset.js
+++ app.js/reset.js
@@ -3,6 +3,7 @@
 
     const clickHandler = () => {
         $counter.textContent = 0;
+        $counter.style.color = "#000000";
     }
 
     document.getElementById("js-reset-button").addEventListener("click", clickHandler)
